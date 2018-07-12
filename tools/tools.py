39e79bdbcb08d49be39cc70c1730909055fa673a
davgares <41119614+davgares@users.noreply.github.com>
2018-07-12 19:57:01 +0200

    [FEATURE] - Common side in polygons
    New functions to identify common side in polygons

--- tools/tools.py
+++ tools/tools.py
@@ -24,3 +24,34 @@ def characterize_neighbours(pindex, neighbours, points,  method='euclidean'):
     index_min_distance = np.argmin(distances, axis=1)
 
     return distances[0], index_max_distance[0], max_distance, index_min_distance[0], min_distance
+
+
+def polygon_2_linestring(poly):
+
+    # Transform a shapely.Polygon into a shapely.LineString
+    return LineString(list(poly.exterior.coords))
+
+
+def identify_common_side_polygons(poly1, poly2):
+
+    polylinestring1 = polygon_2_linestring(poly1)
+    polylinestring2 = polygon_2_linestring(poly2)
+
+    return polylinestring1.intersection(polylinestring2)
+
+
+def create_polygon_triangle_side(poly, side_linestring):
+    
+    if side_linestring.geom_type == 'LineString':
+    
+        center_poly = poly.representative_point()
+        p1 = Point(side_linestring.coords[0])
+        p2 = Point(side_linestring.coords[1])
+        pointlist = [center_poly, p1, p2, center_poly]
+        poly = Polygon([[p.x, p.y] for p in pointlist])
+    
+    else:
+        
+        poly = loads('POLYGON EMPTY')
+    
+    return poly
